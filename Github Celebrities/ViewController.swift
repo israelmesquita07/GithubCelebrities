72ca9e6e4e993340bd6b82ee47367c68b9f7c89a
Israel Mesquita <israelmesquita07@gmail.com>
2020-02-18 15:37:28 -0300

    Initial Commit

---
+++ Github Celebrities/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Github Celebrities
+//
+//  Created by Israel on 18/02/20.
+//  Copyright © 2020 israel3D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
