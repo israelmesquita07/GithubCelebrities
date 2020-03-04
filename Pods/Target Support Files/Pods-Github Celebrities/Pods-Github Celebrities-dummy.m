c83de1e6395466b13aee9f2b048c6aba04bdd977
Israel Mesquita <israelmesquita07@gmail.com>
2020-03-04 15:24:36 -0300

    fastlane

---
+++ Pods/Target Support Files/Pods-Github Celebrities/Pods-Github Celebrities-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Github_Celebrities : NSObject
+@end
+@implementation PodsDummy_Pods_Github_Celebrities
+@end
---
+++ Pods/Target Support Files/Pods-Github CelebritiesTests/Pods-Github CelebritiesTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Github_CelebritiesTests : NSObject
+@end
+@implementation PodsDummy_Pods_Github_CelebritiesTests
+@end
